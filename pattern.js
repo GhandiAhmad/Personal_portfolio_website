6c12c65094befd0cfa06c383dd00ebb9e4fe311a
GhandiAhmad <147191821+GhandiAhmad@users.noreply.github.com>
2023-10-06 21:12:10 -0800

    Add files via upload

---
+++ pattern.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+   $("div").click(function () {
+    $('.predict_para').toggleClass('big'); 
+    $('.predict_para1').toggleClass('medium'); })
+    $("#cong").click(function(){
+        $("#image").fadeIn(3000);
+        
+      });
+     });
+
---
+++ projectscript.js
@@ -0,0 +1,53 @@
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+/////////////////
+
+
+
+
+function ageCalculation() {
+        
+    var input =  document.getElementById("DOB").value;
+    var dob = new Date(input);
+    var xx = dob.getTime();
+    var dayy= dob.getDate()+1;
+    var monthy= dob.getMonth()+1;
+    if (dayy==32){dayy=1; monthy=monthy+1};
+    if (dayy==31 && (monthy==4||monthy==6||monthy==9||monthy==11)){dayy=1; monthy=monthy+1};
+    if ((dayy==29 || dayy==30 ||dayy==31) && (monthy==2)){dayy=1; monthy=monthy+1};
+      document.getElementById("day").innerHTML =dayy;
+      document.getElementById("month").innerHTML =monthy;
+    var yeary= dob.getFullYear();
+      document.getElementById("year").innerHTML = yeary ;
+    var today=new Date()
+    var dd=today.getTime();
+    var  ConverterNum = 1000 * 60 * 60 * 24;
+    var  Result = Math.round((dd - xx) / ConverterNum);
+    return  document.getElementById("result").innerHTML ="Calculated Age in Days is: 🎉" + Result + "🎉days.";
+    
+}
+
